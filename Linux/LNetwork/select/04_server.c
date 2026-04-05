7d7fceda1f956e3aba9786444764bccb230a79a8
Wunchia <wunchia@outlook.com>
2026-04-05 22:29:19 +0800

    socket

--- Linux/LNetwork/select/04_server.c
+++ Linux/LNetwork/select/04_server.c
@@ -3,7 +3,83 @@
 /*Usage: */
 int main(int argc,char *argv[])
 {
+    char *ip="192.168.171.128";
+    char *port="12345";
     
+    int listen_fd=socket(AF_INET,SOCK_STREAM,0);
+    ERROR_CHECK(listen_fd,-1,"socket");
+
+    int opt=1;
+    int ret=setsockopt(listen_fd,SOL_SOCKET,SO_REUSEPORT,&opt,sizeof(opt));
+
+    struct sockaddr_in addr,client_addr;
+    memset(&addr,0,sizeof(addr));
+    socklen_t len=sizeof(client_addr);
+    addr.sin_family=AF_INET;
+    addr.sin_addr.s_addr=inet_addr(ip);
+    addr.sin_port=htons(atoi(port));
+
+    //绑定ip、端口号和listen_fd
+    ret=bind(listen_fd,(struct sockaddr*)&addr,sizeof(addr));
+    ERROR_CHECK(ret,-1,"bind");
+
+    //监听
+    ret=listen(listen_fd,10);
+    ERROR_CHECK(ret,-1,"listen");
+
+    printf("server is listening\n");
+
+    int conn_fd=0;
+    fd_set set;
+    FD_ZERO(&set);
+
+    FD_SET(listen_fd,&set);//将listen_fd监听端口放入位图
+
+    while(1){
+        fd_set tmp;
+        tmp=set;
+        int nready=select(10,&tmp,NULL,NULL,NULL);
+        ERROR_CHECK(nready,-1,"select");
+        printf("----nready:%d----\n",nready);
+
+        //全链接队列中存在就绪消息 存在已完成三次握手的链接
+        if(FD_ISSET(listen_fd,&tmp)){
+            conn_fd=accept(listen_fd,(struct sockaddr*)&client_addr,&len);
+            ERROR_CHECK(conn_fd,-1,"accept");
+
+            FD_SET(conn_fd,&set);
+            FD_SET(STDIN_FILENO,&set);
+
+            FD_CLR(listen_fd,&set);
+        }
+
+        //服务器在自己终端上输入 向客户端传递消息
+        if(FD_ISSET(STDIN_FILENO,&tmp)){
+            char buf[50]={0};
+            read(STDIN_FILENO,buf,sizeof(buf));
+            send(conn_fd,buf,strlen(buf),0);
+        }
+
+        //客户端发来消息
+        if(FD_ISSET(conn_fd,&tmp)){
+            char buf[50]={0};
+            int cnt=recv(conn_fd,buf,sizeof(buf),0);
+            ERROR_CHECK(cnt,-1,"recv");
+            printf("---\nrecv cnt:%d\n",cnt);
+            if(0==cnt){
+                printf("client closed\n---\n");
+                FD_CLR(conn_fd,&set);
+                FD_CLR(STDIN_FILENO,&set);
+                FD_SET(listen_fd,&set);
+                close(conn_fd);
+                continue;
+            }
+            printf("msg:%s\n---\n",buf);
+        }
+    }
+
+    close(listen_fd);
+    close(conn_fd);
     return 0;
 }
 
--- Linux/LNetwork/select/05_server.c
+++ Linux/LNetwork/select/05_server.c
@@ -3,7 +3,108 @@
 /*Usage: */
 int main(int argc,char *argv[])
 {
-    printf("Hello world\n");
+    char*ip="192.168.171.128";
+    char*port="12345";
+
+    int listen_fd=socket(AF_INET,SOCK_STREAM,0);
+    ERROR_CHECK(listen_fd,-1,"socket");
+
+    int opt=1;
+    int ret=setsockopt(listen_fd,SOL_SOCKET,SO_REUSEPORT,&opt,sizeof(opt));
+
+    struct sockaddr_in addr,client_addr;
+    memset(&addr,0,sizeof(addr));
+    memset(&client_addr,0,sizeof(client_addr));
+    socklen_t len=sizeof(client_addr);
+
+    addr.sin_family=AF_INET;
+    addr.sin_addr.s_addr=inet_addr(ip);
+    addr.sin_port=htons(atoi(port));
+
+    ret=bind(listen_fd,(struct sockaddr*)&addr,sizeof(addr));
+    ERROR_CHECK(ret,-1,"bind");
+
+    ret=listen(listen_fd,10);
+    ERROR_CHECK(ret,-1,"listen");
+
+    printf("server is listening\n");
+
+    //将不同用户的对应conn_fd用数组管理起来
+    int arr[1024];
+    for(int idx=0;idx<1024;++idx){
+        arr[idx]=0;
+    }//初始化
+
+    int conn_fd=0;
+
+    fd_set set;
+    FD_ZERO(&set);
+    FD_SET(listen_fd,&set);
+
+    FD_SET(STDIN_FILENO,&set);
+
+    while(1){
+        fd_set tmp;
+        tmp=set;
+
+        int nready=select(10,&tmp,NULL,NULL,NULL);
+        ERROR_CHECK(conn_fd,-1,"select");
+        printf("----nready:%d----\n",nready);
+
+        //监听 维护用户链接
+        if(FD_ISSET(listen_fd,&tmp)){
+            conn_fd=accept(listen_fd,(struct sockaddr*)&client_addr,&len);
+            ERROR_CHECK(conn_fd,-1,"accept");
+
+            FD_SET(conn_fd,&set);
+
+            for(int idx=0;idx<1024;++idx){
+                if(0==arr[idx]){
+                    arr[idx]=conn_fd;
+                    break;
+                }
+            }
+        }
+
+        //通信
+
+        //服务端向客户端广播发送
+        if(FD_ISSET(STDIN_FILENO,&tmp)){
+            char buf[50]={0};
+            read(STDIN_FILENO,buf,sizeof(buf));
+            for(int idx=0;idx<1024;++idx){
+                if(arr[idx]>0){
+                    send(arr[idx],buf,strlen(buf),0);
+                }
+            }
+        }
+
+        for(int idx=0;idx<1024;++idx){
+            if(0==arr[idx]){
+                continue;
+            }
+
+            if(FD_ISSET(arr[idx],&tmp)){
+                char buf[50]={0};
+
+                int cnt=recv(arr[idx],buf,sizeof(buf),0);
+                ERROR_CHECK(cnt,-1,"recv");
+                printf("---\nrecv cnt:%d\n",cnt);
+                if(0==cnt){
+                    printf("client closed\n---\n");
+                    FD_CLR(arr[idx],&set);
+                    close(arr[idx]);
+                    arr[idx]=0;
+                    continue;
+                }
+                printf("msg:%s\n---\n",buf);
+            }
+        }
+    }
+
+    close(listen_fd);
+    close(conn_fd);
     return 0;
+
 }
 
