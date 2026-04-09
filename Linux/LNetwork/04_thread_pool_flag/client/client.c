bc3f0d25b8d6ad0c8fcd399d9186cbb582df425b
Wunchia <wunchia@outlook.com>
2026-04-09 09:49:10 +0800

    test

--- Linux/LNetwork/04_thread_pool_flag/client/client.c
+++ Linux/LNetwork/04_thread_pool_flag/client/client.c
@@ -10,6 +10,7 @@ int main(int argc,char *argv[])
     ERROR_CHECK(client_fd,-1,"socket");
 
     struct sockaddr_in addr;
+    memset(&addr,0,sizeof(addr));
     addr.sin_family=AF_INET;
     addr.sin_addr.s_addr=inet_addr(ip);
     addr.sin_port=htons(atoi(port));
